8f01ce7217338db792ef6b8d8a06baf47d952c05
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-04-23 18:32:11 +0530

    build cmd

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
             <StaticImage className="object-cover object-center rounded" alt="hero" src="/static/aaftab.jpg" />
   </div>*/}
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">A Web Developer
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">A Web Developer.
         </h1>
             <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
             <div className="flex justify-center">
