dc16c80a233bad4e68fbd9d31fe4ca4feecb900a
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-04-24 13:30:15 +0530

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,16 +6,18 @@ import Layout from './components/layout';
 const ContactPage = () => {
     return (
         <Layout>
-            <form name="contact" netlify>
-                <section className="text-gray-600 body-font relative">
-                    <div className="container px-5 py-24 mx-auto">
-                        <div className="flex flex-col text-center w-full mb-12">
-                            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-                            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
-                        </div>
-                        <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                            <div className="flex flex-wrap -m-2">
+
+            <section className="text-gray-600 body-font relative">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-12">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+                    </div>
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                        <div className="flex flex-wrap -m-2">
+                            <form name="contact" method='POST' data-netlify="true" data-netlify-honeypot="bot-field">
                                 <div className="p-2 w-1/2">
+
                                     <div className="relative">
                                         <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
                                         <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
@@ -36,16 +38,18 @@ const ContactPage = () => {
                                 <div className="p-2 w-full">
                                     <button type='submit' className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
                                 </div>
-                                <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                                    <a className="text-indigo-500">pinjari.aaftab1@gmail.com</a>
-                                    <p className="leading-normal my-5">Burhanpur, India
+                            </form>
+                            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                                <a className="text-indigo-500">pinjari.aaftab1@gmail.com</a>
+                                <p className="leading-normal my-5">Burhanpur, India
                             </p>
-                                </div>
                             </div>
+
                         </div>
                     </div>
-                </section>
-            </form>
+                </div>
+            </section>
+
         </Layout>
     )
 }
